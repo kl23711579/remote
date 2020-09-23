d8163fb8e14be1d216a19b49a91a6c2a1a64d4e6
kl23711579 <kl23711579@gmail.com>
2020-09-23 20:27:35 +1000

    picture sh

---
+++ runPicture.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -l
+
+#PBS -l ncpus=2
+#PBS -l mem=1gb
+#PBS -l walltime=00:30:00
+
+module load python/3.7.4-gcccore-8.3.0
+source $HOME/712/bin/activate
+python3 $HOME/remote/picture.py
