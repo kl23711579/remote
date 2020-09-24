4d1447231e10dd0d784af673cc7c5782fb813f3b
kl23711579 <kl23711579@gmail.com>
2020-09-24 11:48:34 +1000

    28 clusters

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def get_freq_words(row, words):
 with open(data_path+"Large_preprocess.pkl", "rb") as f:
     df = pickle.load(f)
 
-clusters_number = 19
+clusters_number = 28 
 df = get_clusters(df, clusters_number)
 
 LDA_models = []
